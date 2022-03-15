e0e52e4da24e060f0b5948e080585e5ebd432504
ufombam <ufombam@gmail.com>
2022-03-15 06:28:14 +0100

    charts_updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,31 +2,50 @@ import React from 'react';
 import Menu from '../Menu/Menu';
 import './Home.scss';
 import { Card } from 'react-bootstrap';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 
 
-const data =  {
-    labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+const barData =  {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
     datasets: [{
-        label: '# of Eggs',
-        data: [12, 19, 3, 5, 2, 3, 30],
+        label: 'No of Eggs',
+        fill: false,
+        lineTension: 0.5,
+        data: [450, 350, 550, 750, 600, 700, 800, 250, 100, 300, 200, 500],
         backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
             'rgba(255, 99, 132, 0.2)'
         ],
         borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
+            'rgba(255, 99, 132, 1)'
+        ],
+        borderWidth: 1
+    }]
+}
+
+const barOptions = {
+    title:{
+        display:true,
+        text:'Daily Eggs count',
+        fontSize:20
+    },
+    legend:{
+        display:true,
+        position:'right'
+    }
+}
+
+const lineData =  {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+    datasets: [{
+        label: 'Amount in Naira',
+        fill: false,
+        lineTension: 0.5,
+        data: [300000, 350000, 550000, 750000, 600000, 70000, 800000, 250000, 150000, 300000, 200000, 500000],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
             'rgba(255, 99, 132, 1)'
         ],
         borderWidth: 1
@@ -34,21 +53,20 @@ const data =  {
 }
 
 
-const options = {
-title:{
-    display:true,
-    text:'Daily Eggs count',
-    fontSize:20
-},
-legend:{
-    display:true,
-    position:'right'
-}
+const lineOptions = {
+    title:{
+        display:true,
+        text:'Daily Eggs count',
+        fontSize:20
+    },
+    legend:{
+        display:true,
+        position:'right'
+    }
 }
 
+
 function Home() {
-      
-    
 
     return (
         <div className="home_app">
@@ -64,20 +82,18 @@ function Home() {
                         <Card.Header className='card_header1 fw-bold'>Egg Laying Activity</Card.Header>
                         <Card.Body>
                         <Card.Title>{'Current Week'} </Card.Title>
-                            <Bar options={{options}} data={data} /> 
+                            <Bar options={{barOptions}} data={barData} /> 
                         </Card.Body>
                     </Card>
                     <Card
                         text={'dark'}
-                        style={{ width: '30rem', border: '1px solid purple' }}
+                        style={{ width: '30rem', height: '20rem', border: '1px solid purple' }}
                         className="mb-2 shadow"
                     >
-                        <Card.Header className='card_header1 fw-bold'>Weekly Income</Card.Header>
+                        <Card.Header className='card_header1 fw-bold'>Monthly Income</Card.Header>
                         <Card.Body>
-                        <Card.Title>{'success'} Card Title </Card.Title>
-                        <Card.Text>
-                            
-                        </Card.Text>
+                        <Card.Title>{'Current Month'} </Card.Title>
+                            <Line options={lineOptions} data={lineData}/>
                         </Card.Body>
                     </Card>
                     <Card
@@ -97,20 +113,7 @@ function Home() {
                 <div className='my_card_container2'>
                     <Card
                         text={'dark'}
-                        style={{ width: '26rem', border: '1px solid orange' }}
-                        className="mb-2 shadow"
-                    >
-                        <Card.Header className='card_header2 fw-bold'>Article</Card.Header>
-                        <Card.Body>
-                        <Card.Title>{'success'} Card Title </Card.Title>
-                        <Card.Text>
-                            
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card
-                        text={'dark'}
-                        style={{ width: '32rem', border: '1px solid orange' }}
+                        style={{ width: '25rem', border: '1px solid orange' }}
                         className="mb-2 shadow"
                     >
                         <Card.Header className='card_header2 fw-bold'>News</Card.Header>
@@ -123,7 +126,7 @@ function Home() {
                     </Card>
                     <Card
                         text={'dark'}
-                        style={{ width: '30rem', border: '1px solid orange' }}
+                        style={{ width: '25rem', border: '1px solid orange' }}
                         className="mb-2 shadow"
                     >
                         <Card.Header className='card_header2 fw-bold'>Events (Todo)</Card.Header>
